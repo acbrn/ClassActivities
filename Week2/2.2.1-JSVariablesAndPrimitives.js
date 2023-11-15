65bc13b075cd35dbfa0e0698397a0cd606056fe4
Andrew Coburn <coburn188@gmail.com>
2023-11-14 20:44:32 -0600

    create week2 folder; section 2.2.1 activity

---
+++ Week2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,22 @@
+// create a constant variable (const)
+const name = "Andrew"
+// create a variable that can be reassigned (let)
+let language = "French"
+console.log(typeof language)
+// create three variables and assign them values of different data types (=)
+let age = 35
+const whatIf = true
+const fancyPants = "Cats PJ"
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof language)
+console.log(typeof age)
+// change the value referenced by a variable (dynamic typing)
+language = 9
+// print the type of the variable you just changed (typeof)
+console.log(typeof language)
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+console.log("Hello, my name is " + name + ", and I am learning " + language + ". I have been practicing for only " +  age + " weeks, so my status as a master is: " + whatIf + ".");
