b8d3c496be8fa81ff443333f413b6f50e3b01b38
Andrew Coburn <coburn188@gmail.com>
2023-11-20 19:52:29 -0600

    Clean up code

--- Week3/3.1.2-FunctionInvocation.js
+++ Week3/3.1.2-FunctionInvocation.js
@@ -11,4 +11,4 @@ console.log(myNumberFunction);
 // now, invoke the myNumberFunction function
 myNumberFunction();
 // print just the function of console.log (no invocation)
-myNumberFunction;
+console.log(console.log());
