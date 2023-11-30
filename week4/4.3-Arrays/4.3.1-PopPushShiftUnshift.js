5fce01f17fdda9c80bea75dcb53084ea0e115f25
Andrew Coburn <coburn188@gmail.com>
2023-11-29 19:25:45 -0600

    4.3 Array Activity

---
+++ week4/4.3-Arrays/4.3.1-PopPushShiftUnshift.js
@@ -0,0 +1,15 @@
+const nums = [6, 5, 2, 3, 4, 1, 0];
+// remove each of the last two items with pop(), saving each item to a variable
+const pop1 = nums.pop();
+const pop2 = nums.pop();
+
+console.log(nums);
+// remove each of the first two items with shift(), saving each item to a variable
+const shift1 = nums.shift();
+const shift2 = nums.shift();
+console.log(nums);
+
+// use push and unshift to add the variables back to the array in numerical order, 0-6
+nums.unshift(pop1, pop2);
+nums.push(shift2, shift1);
+console.log(nums);
